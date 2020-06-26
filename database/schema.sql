9057d1ad3ae58d87edc81d01df8a8a589572d5b0
Chisoo Song <cltn39@gmail.com>
2020-06-26 16:49:57 -0700

    added login schema for database

--- database/schema.sql
+++ database/schema.sql
@@ -4,7 +4,7 @@ USE cardgame_db;
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
-    UserID int NOT NULL Identity(1,1),
+    UserID int NOT NULL,
     SupervisorUserID VARCHAR(50),
     UserGenderID int NOT NULL DEFAULT((0)),
     AccountName nvarchar(50),
@@ -24,21 +24,21 @@ CREATE TABLE user (
     CreatedByUserID int,
     CreatedDate datetime NOT NULL DEFAULT(getdate()),
     ModifiedByUserID int,
-    ModifiedDate datetime NOT NULL DEFAULT(getdate())
+    ModifiedDate datetime NOT NULL DEFAULT(getdate()),
     PRIMARY KEY (UserID)
-)
+);
 
-DROP TABLE IF EXISTS UserUserRole;
+DROP TABLE IF EXISTS Memberships;
 
-CREATE TABLE UserUserRole (
-    UserRoleID int NOT NULL,
+CREATE TABLE Memberships (
     UserID int NOT NULL,
+    UserRoleID int NOT NULL,
     BeginDate  datetime NOT NULL DEFAULT(getdate()),
     EndDate  datetime NOT NULL DEFAULT(getdate()),
     CreatedByUserID int,
-    CreatedDate  datetime NOT NULL DEFAULT(getdate())
+    CreatedDate  datetime NOT NULL DEFAULT(getdate()),
     PRIMARY KEY (UserRoleID, UserID)
-)
+);
 
 DROP TABLE IF EXISTS UserRole;
 
@@ -50,5 +50,55 @@ CREATE TABLE UserRole (
     CreatedByUserID int,
     CreatedDate datetime NOT NULL DEFAULT(getdate()),
     ModifiedByUserID int,
-    ModifiedDate datetime NOT NULL DEFAULT(getdate())
+    ModifiedDate datetime NOT NULL DEFAULT(getdate()),
     PRIMARY KEY (UserRoleID)
+    );
+
+Drop TABLE IF EXISTS UserUserNamePrefix;
+
+CREATE TABLE UserUserNamePrefix (
+    UserID int NOT NULL,
+    UserNamePrefixID int NOT NULL,
+    CreatedByUserID int,
+    CreatedDate datetime DEFAULT(getdate()),
+    PRIMARY KEY (UserID, UserNamePrefixID) NOT NULL
+);
+
+Drop TABLE IF EXISTS UserNamePrefix;
+
+CREATE TABLE UserNamePrefix (
+    UserNamePrefixID int,
+    UserNamePrefix nvarchar(50),
+    UserNamePrefixNote nvarchar(250),
+    Active BOOLEAN DEFAULT 1,
+    CreatedByUserID int,
+    CreatedDate datetime DEFAULT(getdate()) NOT NULL,
+    ModifiedByUserID int,
+    ModifiedDate datetime DEFAULT(getdate()) NOT NULL,
+    PRIMARY KEY (UserNamePrefixID)
+);
+
+DROP TABLE IF EXISTS LoginAttempt;
+
+CREATE TABLE LoginAttempt (
+    LoginAttemptID int NOT NULL,
+    AccountName nvarchar(50) NOT NULL,
+    Password nvarchar(50),
+    IPNumber nvarchar(50),
+    BrowserType nvarchar(200),
+    Success BOOLEAN DEFAULT 0 NOT NULL,
+    CreatedDate datetime DEFAULT(getdate()) NOT NULL,
+    PRIMARY KEY (LoginAttemptID)
+);
+
+DROP TABLE IF EXISTS UserGender;
+
+CREATE TABLE UserGender (
+    UserGenderID int NOT NULL,
+    GenderName nvarchar(50) NOT NULL,
+    CreatedByUserID int,
+    CreatedDate datetime DEFAULT(getdate()) NOT NULL,
+    ModifiedByUserID int,
+    ModifiedDate datetime DEFAULT(getdate()) NOT NULL,
+    PRIMARY KEY (UserGenderID)
+);
