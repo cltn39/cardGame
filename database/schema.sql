b6dd9d689491c7f1000e923bf178a9a1b586877d
Chisoo Song <cltn39@gmail.com>
2020-06-26 12:04:50 -0700

    added schema strcture for user auth

--- database/schema.sql
+++ database/schema.sql
@@ -1,13 +1,54 @@
 CREATE DATABASE IF NOT EXISTS cardgame_db;
 USE cardgame_db;
 
-DROP TABLE IF EXISTS userAuth;
+DROP TABLE IF EXISTS user;
 
-CREATE TABLE userAuth (
-    id int NOT NULL AUTO_INCREMENT,
-    first_name varchar(30) NOT NULL,
-    last_name varchar(50) NOT NULL,
-    passwords int NOT NULL,
-    isAUth BOOL DEFAULT false,
-    PRIMARY KEY (id)
+CREATE TABLE user (
+    UserID int NOT NULL Identity(1,1),
+    SupervisorUserID VARCHAR(50),
+    UserGenderID int NOT NULL DEFAULT((0)),
+    AccountName nvarchar(50),
+    CommonName nvarchar(100),
+    GivenName nvarchar(100),
+    MiddleName nvarchar(100),
+    FamilyName nvarchar(100),
+    FullName nvarchar(300),
+    UserNote nvarchar(500),
+    ExternalUser bit NOT NULL DEFAULT((0)),
+    Photo varbinary,
+    Address1 nvarchar(50),
+    Address2 nvarchar(50),
+    City nvarchar(50),
+    Region nvarchar(50),
+    Active bit NOT NULL DEFAULT((1)),
+    CreatedByUserID int,
+    CreatedDate datetime NOT NULL DEFAULT(getdate()),
+    ModifiedByUserID int,
+    ModifiedDate datetime NOT NULL DEFAULT(getdate())
+    PRIMARY KEY (UserID)
 )
+
+DROP TABLE IF EXISTS UserUserRole;
+
+CREATE TABLE UserUserRole (
+    UserRoleID int NOT NULL,
+    UserID int NOT NULL,
+    BeginDate  datetime NOT NULL DEFAULT(getdate()),
+    EndDate  datetime NOT NULL DEFAULT(getdate()),
+    CreatedByUserID int,
+    CreatedDate  datetime NOT NULL DEFAULT(getdate())
+    PRIMARY KEY (UserRoleID, UserID)
+)
+
+DROP TABLE IF EXISTS UserRole;
+
+CREATE TABLE UserRole (
+    UserRoleID int NOT NUll Identity(1,1),
+    UserRoleName nvarchar(50) NOT NULL,
+    UserRoleNote nvarchar(250),
+    Active bit NOT NULL DEFAULT((1)),
+    CreatedByUserID int,
+    CreatedDate datetime NOT NULL DEFAULT(getdate()),
+    ModifiedByUserID int,
+    ModifiedDate datetime NOT NULL DEFAULT(getdate())
+    PRIMARY KEY (UserRoleID)
--- database/seeds.sql
+++ database/seeds.sql
@@ -1,1 +1,3 @@
+use cardgame_db;
 INSERT INTO userAuth (first_name, last_name, passwords) VALUES ('John', 'Smith', '123456') 
+
